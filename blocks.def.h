ff8c730a1596233d13e9783442e75827fe9ce620
nimai <nimai.m.patel@gmail.com>
2021-04-21 15:59:45 +0530

    added network montior script and reformating

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",    "echo ''",     0,      1},
-	{"",	"battery",	5,	3},
-	{"",	"volume",	0,	10},
-	{"",	"internet",	5,	4},
-	{"",	"clock",	60,	1},
+	{"",    "printf ' ' && battery",         5,    3},
+	{"",    "volume",          0,    10},
+	{"",    "network-traffic", 1,    1},
+	{"",    "internet",        5,    4},
+	{"",     "clock",          60,   1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
