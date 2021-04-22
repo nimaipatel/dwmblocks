7b2cd5c79b6ab1ba3c550c947b221d9df6fe777b
nimai <nimai.m.patel@gmail.com>
2021-04-22 10:46:11 +0530

    use prefix to avoid name conflicts for modules

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"",    "printf ' ' && battery",         5,    3},
-	{"",    "volume",          0,    10},
-	{"",    "network-traffic", 1,    1},
-	{"",    "internet",        5,    4},
-	{"",     "clock",          60,   1},
+	{"",    "printf ' ' && sb_battery",               5,               3},
+	{"",                      "sb_volume",               0,              10},
+	{"",             "sb_network_traffic",               1,               1},
+	{"",                    "sb_internet",               5,               4},
+	{"",                       "sb_clock",              60,               1},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
